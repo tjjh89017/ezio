7ceeb62ec70bbe3dbc307aca69b002173f981dab
Mango King <yench@cs.nctu.edu.tw>
2016-09-24 22:58:28 -0700

    Read function is finished

--- main.cpp
+++ main.cpp
@@ -38,8 +38,66 @@ struct temp_storage : lt::storage_interface {
 
 	int readv(lt::file::iovec_t const* bufs, int num_bufs, int piece, int offset, int flags, lt::storage_error& ec)
 	{
-		//return preadv(this->fd, bufs, num_bufs, piece * std::uint64_t(m_files.piece_length()) + offset);
-		return 0;
+		int index = 0;
+		int i = 0;
+		int ret = 0;
+		unsigned long long device_offset = 0;
+		unsigned long long fd_offset = 0; // A fd' point we read data from fd from 
+		unsigned long long cur_offset = 0; // A pieces' point we have to write data until
+		unsigned long long remain_len = 0;
+		unsigned long long piece_sum = 0;
+		unsigned long long data_len = 0;
+		char *data_buf, *data_ptr = NULL;
+		char filename[33]; // Should be the max length of file name
+
+		// Get file name from offset
+		index = m_files.file_index_at_offset( piece * std::uint64_t(m_files.piece_length()) + offset);
+		memcpy( filename, m_files.file_name_ptr(index), m_files.file_name_len(index));
+		filename[m_files.file_name_len(index)] = 0;
+		sscanf(filename,"%llx", &device_offset);
+
+		// Caculate total piece size of previous files
+		for( i = 0 ; i < index; i++ )
+			piece_sum += m_files.file_size(i);
+
+		// Caculate the length of all bufs
+		for( i = 0 ; i < num_bufs ; i ++){
+			data_len += bufs[i].iov_len;
+		}
+		data_buf = (char *)malloc(data_len);
+
+		// Read fd to data_buf
+		cur_offset = piece * std::uint64_t(m_files.piece_length()) + offset;
+		fd_offset = device_offset + offset + piece * std::uint64_t(m_files.piece_length()) - piece_sum;
+		remain_len = m_files.file_size(index) - (offset + piece * std::uint64_t(m_files.piece_length()) - piece_sum);
+		data_ptr = data_buf;
+		while(data_len > 0){
+			if( data_len > remain_len ){
+				ret += pread(this->fd, data_ptr, remain_len, fd_offset);
+				data_len -= remain_len;
+				data_ptr += remain_len;
+				cur_offset += remain_len;
+				index = m_files.file_index_at_offset( cur_offset );
+				memcpy( filename, m_files.file_name_ptr(index), m_files.file_name_len(index));
+				filename[m_files.file_name_len(index)] = 0;
+				sscanf(filename,"%llx", &fd_offset);
+				remain_len = m_files.file_size(index);
+			}
+			else{
+				ret += pread(this->fd, data_ptr, data_len, fd_offset);
+				data_len -= data_len;
+			}
+		}
+
+		// Copy data_buf to bufs
+		data_ptr = data_buf;
+		for( i = 0 ; i < num_bufs ; i ++){
+			memcpy(bufs[i].iov_base, data_ptr, bufs[i].iov_len);
+			data_ptr += bufs[i].iov_len;
+		}
+
+		free(data_buf);
+		return ret;
 	}
 
 	int writev(lt::file::iovec_t const* bufs, int num_bufs, int piece, int offset, int flags, lt::storage_error& ec)
@@ -48,13 +106,13 @@ struct temp_storage : lt::storage_interface {
 		int i = 0;
 		int ret = 0;
 		unsigned long long device_offset = 0;
-		unsigned long long fd_offset = 0; // A pieces' point we writing data to fd from 
-		unsigned long long cur_offset = 0; // A pieces' point we stoping writing data at
-		unsigned long long remind_len = 0;
+		unsigned long long fd_offset = 0; // A fd' point we write data to fd from 
+		unsigned long long cur_offset = 0; // A pieces' point we have to read data until
+		unsigned long long remain_len = 0;
 		unsigned long long piece_sum = 0;
 		unsigned long long data_len = 0;
 		char *data_buf = NULL, *data_ptr = NULL;
-		char filename[33]; // Should be max length of file name
+		char filename[33]; // Should be the max length of file name
 
 		// Get file name from offset
 		index = m_files.file_index_at_offset( piece * std::uint64_t(m_files.piece_length()) + offset);
@@ -62,7 +120,7 @@ struct temp_storage : lt::storage_interface {
 		filename[m_files.file_name_len(index)] = 0;
 		sscanf(filename,"%llx", &device_offset);
 
-		// Caculate total size of previos files
+		// Caculate total piece size of previous files
 		for( i = 0 ; i < index; i++ )
 			piece_sum += m_files.file_size(i);
 
@@ -79,22 +137,22 @@ struct temp_storage : lt::storage_interface {
 			data_ptr += bufs[i].iov_len;
 		}
 
-		// Writing data to fd
+		// Write data_buf to fd
 		cur_offset = piece * std::uint64_t(m_files.piece_length()) + offset;
 		fd_offset = device_offset + offset + piece * std::uint64_t(m_files.piece_length()) - piece_sum;
-		remind_len = m_files.file_size(index) - (offset + piece * std::uint64_t(m_files.piece_length()) - piece_sum);
+		remain_len = m_files.file_size(index) - (offset + piece * std::uint64_t(m_files.piece_length()) - piece_sum);
 		data_ptr = data_buf;
 		while(data_len > 0){
-			if( data_len > remind_len ){
-				ret += pwrite(this->fd, data_ptr, remind_len, fd_offset);
-				data_len -= remind_len;
-				data_ptr += remind_len;
-				cur_offset += remind_len;
+			if( data_len > remain_len ){
+				ret += pwrite(this->fd, data_ptr, remain_len, fd_offset);
+				data_len -= remain_len;
+				data_ptr += remain_len;
+				cur_offset += remain_len;
 				index = m_files.file_index_at_offset( cur_offset );
 				memcpy( filename, m_files.file_name_ptr(index), m_files.file_name_len(index));
 				filename[m_files.file_name_len(index)] = 0;
 				sscanf(filename,"%llx", &fd_offset);
-				remind_len = m_files.file_size(index);
+				remain_len = m_files.file_size(index);
 			}
 			else{
 				ret += pwrite(this->fd, data_ptr, data_len, fd_offset);
