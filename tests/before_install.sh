f0f66f7a9c3df98a066a39cf1aa38efe38fc9a29
tjjh89017 <tjjh89017@hotmail.com>
2017-04-15 02:38:22 +0800

    Remove sudo required, use configure rather than cmake
    cmake wont work fine with static library

--- tests/before_install.sh
+++ tests/before_install.sh
@@ -2,8 +2,9 @@
 
 # build libtorrent
 wget https://github.com/arvidn/libtorrent/releases/download/libtorrent-1_1_3/libtorrent-rasterbar-1.1.3.tar.gz
-tar xf libtorrent-rasterbar-1.1.3.tar.gz
-cd libtorrent-rasterbar-1.1.3/
-cmake .
-make -j
-sudo make install
+mkdir -p libtorrent-rasterbar && tar xf libtorrent-rasterbar-1.1.3.tar.gz -C libtorrent-rasterbar/ --strip-components=1
+cd libtorrent-rasterbar/
+mkdir -p build
+./configure --prefix=$PWD/build/
+make -j4
+make install
