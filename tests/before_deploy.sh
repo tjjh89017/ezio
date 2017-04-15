a9f655eabaf7b423ff71f1516374a0914df30084
tjjh89017 <tjjh89017@hotmail.com>
2017-04-16 00:43:07 +0800

    Send SSH key to remote

--- tests/before_deploy.sh
+++ tests/before_deploy.sh
@@ -5,4 +5,7 @@ EZIO="static-ezio"
 
 # copy all stuff to remote server
 scp $SSH_ARGS "$EZIO" "ezio@ezio.kojuro.date:~/"
+scp $SSH_ARGS "tests/test_ezio.sh" "ezio@ezio.kojuro.date:~/"
+scp $SSH_ARGS "tests/ezio-openrc.sh" "ezio@ezio.kojuro.date:~/"
+scp $SSH_ARGS "tests/ezio.pem" "ezio@ezio.kojuro.date:~/"
 scp -r $SSH_ARGS "tests/scripts/" "ezio@ezio.kojuro.date:~/"
--- tests/scripts/main.py
+++ tests/scripts/main.py
@@ -3,6 +3,7 @@
 
 import os
 import time
+from subprocess import Popen, PIPE
 
 from keystoneauth1.identity import v3
 from keystoneauth1 import session
@@ -19,6 +20,36 @@ def get_session():
     )
     return session.Session(auth=auth, verify=False)
 
+def ssh(user, ip, cmd, key=None):
+
+    # SSH_ARGS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+    ssh_cmd = ['ssh', '-o', 'UserKnownHostsFile=/dev/null', '-o', 'StrictHostKeyChecking=no']
+    if key:
+        ssh_cmd = ssh_cmd + ['-i', key]
+    ssh_cmd.append('%s@%s' % (user, ip))
+    ssh_cmd = ssh_cmd + cmd
+
+    process = Popen(ssh_cmd, stdout=PIPE, stderr=PIPE)
+    return process
+
+def scp(user, ip, filename, key=None):
+
+    scp_cmd = ['scp', '-o', 'UserKnownHostsFile=/dev/null', '-o', 'StrictHostKeyChecking=no']
+    if key:
+        scp_cmd = scp_cmd + ['-i', key]
+    scp_cmd.append(filename)
+    scp_cmd.append('%s@%s:./' % (user, ip))
+    process = Popen(scp_cmd, stdout=PIPE, stderr=PIPE)
+    return process.wait()
+
+def wait_for_instance(user, ip, key=None):
+
+    while True:
+        process = ssh(user, ip, ['true'], key)
+        if process.wait() == 0:
+            break
+        
+
 def main():
 
     # get config
@@ -28,10 +59,10 @@ def main():
     nova = Client("2.1", session=ses, insecure=True)
 
     # create 10 VM
-    flavor = nova.flavors.find(name='m1.small')
-    image = nova.glance.find_image('Ubuntu 16.04 x86_64')
+    flavor = nova.flavors.find(name='m1.medium')
+    image = nova.glance.find_image('ezio')
     key_name = 'ezio'
-    min_count = 5
+    min_count = 10
 
     nova.servers.create("ezio", image, flavor, key_name=key_name, min_count=min_count)
 
@@ -47,17 +78,33 @@ def main():
     print(ips)
 
     # wait for ssh
+    print("wait for ssh")
+    for ip in ips:
+        wait_for_instance('ubuntu', ip)
 
     # copy stuff to instances
+    print("scp stuff to instances")
+    for ip in ips:
+        scp('ubuntu', ip, 'a.torrent')
+        scp('ubuntu', ip, 'static-ezio')
 
     # run script
+    print("run script")
+    procs = []
+    for ip in ips:
+        process = ssh('ubuntu', ip, ['./static-ezio', '-t', '1', 'a.torrent', '/tmp/target'])
+        procs.append((ip, process))
 
     # collect the result
+    print("wait for result")
+    for ip, process in procs:
+        output = process.communicate()[0]
+        #output = output.replace('\r', '\n')
+        print(ip)
+        print(output)
 
     # delete instances
     list(map(nova.servers.delete, nova.servers.list()))
-
-    pass
 
 if __name__ == '__main__':
     main()
