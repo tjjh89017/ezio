1f5fbe4eb459ab7d465f635191b3694b403f9f38
tjjh89017 <tjjh89017@hotmail.com>
2017-04-15 19:51:19 +0800

    Change dir first & Fix Typo

--- tests/scripts/test_ezio.sh
+++ tests/scripts/test_ezio.sh
@@ -3,8 +3,11 @@
 # NOTE you should install pip, virtualenv, virtualenvwarpper first
 # sudo apt install python3-pip
 # sudo pip3 install virtualenv virtualenvwarpper
+
+cd scripts/
+
 export WORKON_HOME=~/Env
-source /usr/local/bin/virtualenvwarpper.sh
+source /usr/local/bin/virtualenvwrapper.sh
 mkvirtualenv ezio
 pip3 install -r requirements.txt
 
