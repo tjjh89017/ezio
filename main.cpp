c716b361ff0e3c112b96da03e92a77d866833748
tjjh89017 <tjjh89017@hotmail.com>
2019-01-08 13:22:18 +0800

    Bug Fix, If force seeding from disk, O_CREAT will flush disk first
    so data in disk will dead

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ struct raw_storage : lt::storage_interface {
 	// Open disk fd
 	raw_storage(lt::file_storage const& fs, const std::string tp) : m_files(fs), target_partition(tp)
 	{
-		this->fd = open(target_partition.c_str(), O_RDWR | O_CREAT);
+		this->fd = open(target_partition.c_str(), O_RDWR);
 		if(this->fd < 0){
 			// Failed handle
 			std::cerr << "Failed to open " << target_partition << std::endl;
