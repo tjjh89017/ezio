90134643f27f20d2f104822d0ba04f1594f5d395
Date Huang <tjjh89017@hotmail.com>
2022-03-13 21:42:58 +0800

    add buffer_pool
    Signed-off-by: Date Huang <tjjh89017@hotmail.com>

---
+++ buffer_pool.cpp
@@ -0,0 +1,41 @@
+#include "buffer_pool.hpp"
+
+namespace ezio {
+
+buffer_pool::buffer_pool()
+{
+	m_buffer = new char[MAX_BUFFER_POOL_SIZE];
+
+	// put into deque
+	for (int i = 0; i < MAX_BUFFER_POOL_SIZE; i += DEFAULT_BLOCK_SIZE) {
+		m_deque.push_back(m_buffer + i);
+	}
+}
+
+buffer_pool::~buffer_pool()
+{
+	m_deque.clear();
+	delete m_buffer;
+}
+
+char* buffer_pool::allocate_buffer()
+{
+	std::unique_lock<std::mutex> l(m_pool_mutex);
+
+	// no memory
+	if (m_deque.empty()) {
+		return nullptr;
+	}
+
+	char *buf = m_deque.front();
+	m_deque.pop_front();
+	return buf;
+}
+
+void buffer_pool::free_buffer(char *buf)
+{
+	std::unique_lock<std::mutex> l(m_pool_mutex);
+	m_deque.push_back(buf);
+}
+
+} // namespace ezio
---
+++ buffer_pool.hpp
@@ -0,0 +1,32 @@
+#ifndef __BUFFER_POOL_HPP__
+#define __BUFFER_POOL_HPP__
+
+#include <deque>
+#include <mutex>
+
+// 256 MB
+#define MAX_BUFFER_POOL_SIZE (256ULL * 1024 * 1024)
+// 16 KB
+#define DEFAULT_BLOCK_SIZE (16 * 1024)
+
+namespace ezio {
+
+class buffer_pool {
+
+public:
+	buffer_pool();
+	~buffer_pool();
+
+	char* allocate_buffer();
+	void free_buffer(char*);
+
+private:
+	std::mutex m_pool_mutex;
+	char *m_buffer;
+	std::deque<char*> m_deque;
+
+};
+
+} // namespace ezio
+
+#endif
