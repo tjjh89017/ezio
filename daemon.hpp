6fc631539962e76e397e5454b57a0639ff5afc1a
Jia-Jun Yeh <jun.yeh@stranity.com>
2022-04-17 18:56:05 +0800

    replace c pointer with unique_ptr

--- daemon.hpp
+++ daemon.hpp
@@ -1,6 +1,7 @@
 #ifndef __DAEMON_HPP__
 #define __DAEMON_HPP__
 
+#include <atomic>
 #include <libtorrent/libtorrent.hpp>
 #include "service.hpp"
 
@@ -8,28 +9,27 @@ namespace ezio {
 
 class ezio {
 public:
-	static ezio& get_instance();
-	
-	void stop();
-	void wait(int sec);
+  static ezio &get_instance();
 
-	void set_session(lt::session*);
-	lt::session* get_session();
-	void set_grpcservice(gRPCService*);
-	gRPCService* get_grpcservice();
+  void stop();
+  void wait(int sec);
+
+  void set_session(std::unique_ptr<lt::session>);
+  lt::session *get_session();
+  void set_grpcservice(std::unique_ptr<gRPCService>);
+  gRPCService *get_grpcservice();
 
 private:
-	ezio();
-	~ezio() = default;
-	ezio(const ezio&) = delete;
-	ezio& operator=(const ezio&) = delete;
-	
-	bool shutdown = false;
-	lt::session *session;
-	gRPCService *grpcservice;
+  ezio();
+  ~ezio() = default;
+  ezio(const ezio &) = delete;
+  ezio &operator=(const ezio &) = delete;
+
+  std::atomic_bool shutdown_;
+  std::unique_ptr<lt::session> session_;
+  std::unique_ptr<gRPCService> grpcservice_;
 };
 
 } // namespace ezio
-
 
 #endif
